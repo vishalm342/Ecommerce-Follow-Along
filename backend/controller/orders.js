23901cc5f47285cdb31246ddfd947613f20c822a
vishalm342 <vishalm8656@gmail.com>
2025-03-25 12:45:44 +0530

    Milestone 27 & 28

--- backend/controller/orders.js
+++ backend/controller/orders.js
@@ -43,7 +43,24 @@ router.post('/place-order', async (req, res) => {
                 res.status(500).json({ message: error.message });
             }
         });
-
+        router.patch('/cancel-order/:orderId', async (req, res) => {
+            try {
+                const { orderId } = req.params;
+                const order = await Order.findById(orderId);
+                console.log(order);
+                if (!order) {
+                    return res.status(404).json({ message: 'Order not found.' });
+                }
+        
+                order.orderStatus = 'Cancelled';
+                await order.save();
+        
+                res.status(200).json({ message: 'Order cancelled successfully.', order });
+            } catch (error) {
+                console.error('Error cancelling order:', error);
+                res.status(500).json({ message: error.message });
+            }
+        });
         // Create separate orders for each order item
         const orderPromises = orderItems.map(async (item) => {
             const totalAmount = item.price * item.quantity;
@@ -106,5 +123,7 @@ router.post('/place-order', async (req, res) => {
         console.error('Error placing orders:', error);
         res.status(500).json({ message: error.message });
     }
+
+
 });
 module.exports = router;
